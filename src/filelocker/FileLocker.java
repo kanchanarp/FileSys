19f752e2b70336d73a8f899b2882cd88d64d6e78
kanchanarp <kanchana.ruwanpathirana@gmail.com>
2016-06-26 05:04:52 +0530

    biometrics update

--- src/filelocker/FileLocker.java
+++ src/filelocker/FileLocker.java
@@ -14,8 +14,8 @@ public class FileLocker {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+//    public static void main(String[] args) {
+//        // TODO code application logic here
+//    }
     
 }
